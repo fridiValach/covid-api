81ea5f27741f58189ca05965686d984f90805446
fridiValach <97057811+fridiValach@users.noreply.github.com>
2022-07-11 18:40:47 +0300

    Update NavBar.js

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -3,107 +3,94 @@ import Select from "../select/Select";
 import "./NavBar.css";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 
-const NavBar = ({ countries }) => {
+const NavBar = ({ countries, isClick, setIsClick }) => {
   function clickFunc(name) {
     navigate("../" + name);
     setValue(name);
     setIsClick(false);
   }
 
-  const date= new Date();
-  const a=date.toDateString()
+  const date = new Date();
+  const a = date.toDateString();
   const navigate = useNavigate();
   const [mapArr, setMapArr] = useState(countries);
-  const [isClick, setIsClick] = useState(false);
-  //const [isSet, setIsSet] = useState(false);
+
   const [value, setValue] = useState("");
-  console.log(mapArr, "mapArr");
-   useEffect(() => {
-   setMapArr(countries)
-     }, [countries]);
-  
+  useEffect(() => {
+    setMapArr(countries);
+  }, [countries]);
   useEffect(() => {
     const newCountries = countries.filter((el) =>
       el.name.toLowerCase().includes(value?.toLowerCase())
     );
     setMapArr(newCountries);
-    //if (mapArr.length < 1){setIsSet(true)}
-
-    console.log(newCountries);
-    console.log(value);
-    console.log(countries);
   }, [value]);
-
 
   return (
     <div>
       <h3 className="covidTitle">COVID 19 CORONAVIRUS TRACKER</h3>
-    <div className="link">
-      <p>
-        <Link to={"/"} style={{ "text-decoration": "none" }}>
-          home
-        </Link>
-      </p>
-      <div
-        onFocus={() => {
-          setIsClick(true);
-          //isSet && setMapArr([...countries]);
-          // mapArr.length < 1&& setIsSet(true)
-
-          console.log(isClick);
-        }}
-      >
-        {isClick && (
-          <button
-            onClick={() => {
-              setIsClick(false);
-              setValue("");
-            }}
-          >
-            X
-          </button>
-        )}
-        <input
-          type="text"
-          placeholder="country"
-          value={value}
-          onChange={(e) => {
-            setValue(e.target.value);
+      <div className="link">
+        <p>
+          <Link to={"/"} style={{ textDecoration: "none" }}>
+            home
+          </Link>
+        </p>
+        <div
+          onFocus={() => {
             setIsClick(true);
           }}
-          onKeyDown={(e) => {
-            console.log(e.key);
-            if (e.key === "Enter") {
-              if (mapArr.length===1){
-              clickFunc(mapArr[0].name)}
-            } else if (e.key === "ArrowDown") {
-            } else if (e.key === "ArrowUp") {
-            }
-          }}
-        ></input>
+        >
+          {isClick && (
+            <button
+              onClick={() => {
+                setIsClick(false);
+                setValue("");
+              }}
+            >
+              X
+            </button>
+          )}
+          <input
+            type="text"
+            placeholder="country"
+            value={value}
+            onChange={(e) => {
+              setValue(e.target.value);
+              setIsClick(true);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                /*בתחילה רשמתי כך שרק אם יש ערך בודד שתואם להקלדה- זה יעבוד, אך אח"כ שיניתי שיעבוד תמיד עם הערך הראשון התואם. מה נכון יותר?
+                if (mapArr.length === 1) {
+                  clickFunc(mapArr[0].name);
+                }*/
+                clickFunc(mapArr[0].name);
+              }
+            }}
+          ></input>
 
-        {isClick && (
-          <div className={`${isClick && "display"}`}>
-            {mapArr.map((country, i) => (
-              <Select
-                clickFunc={clickFunc}
-                i={i}
-                name={country.name}
-                setValue={setValue}
-                setIsClick={setIsClick}
-                code={country.code}
-                key={country.code}
-              />
-            ))}
-          </div>
-        )}
-      </div>
+          {isClick && (
+            <div className={`${isClick && "display"}`}>
+              {mapArr.map((country, i) => (
+                <Select
+                  clickFunc={clickFunc}
+                  i={i}
+                  name={country.name}
+                  setValue={setValue}
+                  setIsClick={setIsClick}
+                  code={country.code}
+                  key={country.code}
+                />
+              ))}
+            </div>
+          )}
+        </div>
 
-      <p>
-        <Link to={"/about"} style={{ "text-decoration": "none" }}>
-          about
-        </Link>
-      </p>
+        <p>
+          <Link to={"/about"} style={{ textDecoration: "none" }}>
+            about
+          </Link>
+        </p>
       </div>
       <h4>Correct data for: {a}</h4>
     </div>
