fda6296ed7d852278e42bb5020cc4fbc305629b4
fridiValach <97057811+fridiValach@users.noreply.github.com>
2022-07-11 11:00:36 +0300

    Update CountryData.js

--- src/components/CountryData.js
+++ src/components/CountryData.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 const CountryData = ({dataName, number }) => {
   return (
-    <div
+    <div>
        <h4>{dataName}</h4>
               <h5>{number}</h5>
     </div>
